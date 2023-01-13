bc7bad4369c9ca1bd2aa1acd16e0fec6819e4530
xayananh4 <xayananh4@gmail.com>
2023-01-13 14:03:06 -0800

    add reset button to clear previous answers

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@
 
 
 function handleSubmission(event) {
-  event.preventDefault();
+  
   let python  = document.querySelector("div#python");
   let javascript  = document.querySelector("div#javascript");
   let cplus = document.querySelector("div#cplus");
-  
   let selectionFromInput01 = parseInt(document.getElementById('input1').value);
   let selectionFromInput02 = parseInt(document.getElementById('input2').value);
   let selectionFromInput03 = parseInt(document.getElementById('input3').value);
@@ -16,7 +15,6 @@ function handleSubmission(event) {
   let totalValueFromAllInputs = selectionFromInput01 + selectionFromInput02 + 
                         selectionFromInput03 + selectionFromInput04 + 
                         selectionFromInput05;
-
 
   if(totalValueFromAllInputs < 25)
   {
@@ -31,17 +29,35 @@ function handleSubmission(event) {
   else {    
       document.getElementById("error").innerHTML = "Please enter in values!";
   }
-    
-
  
 }
 
 
+window.addEventListener("load", function() {  
+  let form = document.querySelector("form");
+  let python  = document.querySelector("div#python");
+  let javascript  = document.querySelector("div#javascript");
+  let cplus = document.querySelector("div#cplus");
+  let resetBtn = document.querySelector("button#reset");
+  form.addEventListener("submit", function(event) {
+    handleSubmission();
+    
+    reset.removeAttribute("class");
+    event.preventDefault();
+  });
 
+     
+   // Reset text box values
+  resetBtn.addEventListener("click", function() {
+    python.setAttribute("class", "defaultSettingHidden");
+    javascript.setAttribute("class", "defaultSettingHidden");
+    cplus.setAttribute("class", "defaultSettingHidden");
+    document.getElementById("input1").value = null;
+    document.getElementById("input2").value = null;
+    document.getElementById("input3").value = null;
+    document.getElementById("input4").value = null;
+    document.getElementById("input5").value = null;
 
-
-window.addEventListener("load", function() {
-  const form = document.getElementById("surveyform");
-  form.addEventListener("submit", handleSubmission);
+  });
  
 });
